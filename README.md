bd824d8d56abc4fd335987fb73ea341e39e0042b
syjeon <jsyu3799@gmail.com>
2022-08-19 18:09:45 +0900

    show as table

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 # model-benchmark
-this repo is a simple code for evaluating pytorch model (params, flops, fps)
+this repo is a simple code for evaluating pytorch [timm](https://github.com/rwightman/pytorch-image-models) models (params, flops, fps)
 this repo used [ptflops](https://github.com/sovrasov/flops-counter.pytorch)
 
 ## bechmark
 
 CPU: Intel(R) Core(TM) i5-1038NG7 CPU @ 2.00GHz CPU and no GPU.
 
-| Model     | resolution | #Params(M) | GFLOPs | FPS |
+| Model     | Resolution | Params(M) | GFLOPs | FPS |
 | :-------- | :--------: | :--------: | :----: | :---------:|
-| convnext_base | 224 | 88.5 | 15.4 | 4.2 |
-| cspdarknet53 | 256 | 27.6 | 6.5 | 7.8 |
+| convnext_nano | (3, 224, 224) | 15.59 | 2.46 | 16.8 |
+| convnext_nano_hnf | (3, 224, 224) | 15.59 | 2.46 | 16.9 |
+| convnext_nano_ols | (3, 224, 224) | 15.61 | 2.51 | 16.2 |
 
 ## example
 
-python3 eval.py
+python3 eval.py --device cpu --iter 20
 ```
-Model: cspdarknet53
-Input size: (3, 256, 256)
-Computational complexity:       6.55 GMac
-Number of parameters:           27.64 M 
-Average prediction time: 128.6 ms (7.775 fps)
+Model                          Resolution       Params     GFLOPs      FPS       
+convnext_nano                  [(3, 224, 224), '15.59 M', '2.46 GMac', 16.8]
+convnext_nano_hnf              [(3, 224, 224), '15.59 M', '2.46 GMac', 16.9]
+convnext_nano_ols              [(3, 224, 224), '15.61 M', '2.51 GMac', 16.2]
 ```
--- eval.py
+++ eval.py
@@ -6,24 +6,30 @@ from ptflops import get_model_complexity_info
 
 def benchmark(args):
     ## add model list
-    model_list = timm.list_models('convnext*')
-    [print(model) for model in model_list]
+    model_list = timm.list_models('convnext_nano*')
+    # [print(model) for model in model_list]
 
+    model_table = {}
     for model_name in model_list:
+
+        model_table[model_name] = []
 
         ## create model
         model = timm.create_model(model_name)
         model.to(args.device)
         input_size = model.default_cfg['input_size']
-        print(f"Model: %s" % model_name)
-        print(f"Input size: %s" % (input_size,))
+        model_table[model_name].append(input_size)
+        # print(f"Model: %s" % model_name)
+        # print(f"Input size: %s" % (input_size,))
 
         ## calculate params, flops
         ## https://github.com/sovrasov/flops-counter.pytorch
         macs, params = get_model_complexity_info(model, input_size, as_strings=True,
                                                 print_per_layer_stat=False, verbose=False)
-        print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
-        print('{:<30}  {:<8}'.format('Number of parameters: ', params))
+        model_table[model_name].append(params)
+        model_table[model_name].append(macs)
+        # print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
+        # print('{:<30}  {:<8}'.format('Number of parameters: ', params))
 
         ## calculate fps
         dummy_input = torch.randn(input_size, device=args.device)
@@ -34,8 +40,14 @@ def benchmark(args):
             results = model(dummy_input)
             times.append((time.time()-starting_time)*1000)
         avg_time = sum(times) / len(times)
-        fps = 1000 / avg_time
-        print(f"Average prediction time: {avg_time:.1f} ms ({fps:.3f} fps)")
+        fps = round(1000 / avg_time, 1)
+        model_table[model_name].append(fps)
+        # print(f"Average prediction time: {avg_time:.1f} ms ({fps:.3f} fps)")
+    
+    print("{:<30} {:<16} {:<10} {:<11} {:<10}".format('Model','Resolution','Params','GFLOPs', 'FPS'))
+    for k, v in model_table.items():
+        print("{:<30} {:<10}".format(k, str(v)))
+
 
 if __name__ == "__main__":
 
