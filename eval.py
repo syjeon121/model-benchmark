c49ea35dae78b216f6de819fa243889b9aefc17d
syjeon <jsyu3799@gmail.com>
2022-08-23 22:43:21 +0900

    sort by fps

--- eval.py
+++ eval.py
@@ -9,15 +9,16 @@ def benchmark(args):
     model_list = timm.list_models('convnext_nano*')
     # [print(model) for model in model_list]
 
-    model_table = {}
+    model_table = []
     for model_name in model_list:
 
         ## create model
-        model_table[model_name] = []
+        model_info = []
+        model_info.append(model_name)
         model = timm.create_model(model_name)
         model.to(args.device)
         input_size = model.default_cfg['input_size']
-        model_table[model_name].append(input_size)
+        model_info.append(input_size)
         # print(f"Model: %s" % model_name)
         # print(f"Input size: %s" % (input_size,))
 
@@ -43,8 +44,8 @@ def benchmark(args):
         ## https://github.com/sovrasov/flops-counter.pytorch
         macs, params = get_model_complexity_info(model, input_size, as_strings=True,
                                                 print_per_layer_stat=False, verbose=False)
-        model_table[model_name].append(params)
-        model_table[model_name].append(macs)
+        model_info.append(params)
+        model_info.append(macs)
         # print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
         # print('{:<30}  {:<8}'.format('Number of parameters: ', params))
 
@@ -56,12 +57,17 @@ def benchmark(args):
             times.append((time.time()-starting_time)*1000)
         avg_time = sum(times) / len(times)
         fps = round(1000 / avg_time, 1)
-        model_table[model_name].append(fps)
+        model_info.append(fps)
         # print(f"Average prediction time: {avg_time:.1f} ms ({fps:.3f} fps)")
-    
+
+        model_table.append(model_info)
+
+    ## sort by fps    
+    model_table.sort(key = lambda x: x[-1], reverse=True)
+
     print("{:<30} {:<16} {:<10} {:<11} {:<10}".format('Model','Resolution','Params','GFLOPs', 'FPS'))
-    for k, v in model_table.items():
-        print("{:<30} {:<10}".format(k, str(v)))
+    for model_info in model_table:
+        print("{:<30} {:<16} {:<10} {:<11} {:<10}".format(model_info[0], str(model_info[1]), model_info[2], model_info[3], model_info[4]))
 
 
 if __name__ == "__main__":
